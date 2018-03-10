4523e2bea309908b6e88388c6b58c1918d942787
PawelMacan <pawelm86@gmail.com>
2018-03-10 12:38:36 +0100

    Refactoring

--- FInal_Project/side_projects/tanks.py
+++ FInal_Project/side_projects/tanks.py
@@ -8,12 +8,13 @@ main_dir = os.path.split(os.path.abspath(__file__))[0]
 
 class Enemy(object):
 
-    def __init__(self, dir_x,dir_y):
+    def __init__(self, dir_x, dir_y):
         self.alive = True
         rand_loc = random.randint(0, screen_width - 64)
         rand_loc_2 = random.randint(0, screen_height - 180)
 
-        self.rect = pygame.Rect(rand_loc, rand_loc_2, 16, 16)
+
+        self.rect = pygame.Rect(rand_loc, rand_loc_2, tank_size, tank_size)
         self.dir_x = dir_x
         self.dir_y = dir_y
 
@@ -39,19 +40,7 @@ class Enemy(object):
 
 class Player(object):
     def __init__(self):
-        # pygame.sprite.Sprite.__init__(self)
-        # self.image = pygame.Surface((16, 16))
-        # self.image.fill(green)
-        # self.rect = self.image.get_rect()
-        # self.rect.centerx = screen_width / 2
-        # self.rect.bottom = screen_height /2
-        # self.speedx = 5
-
-
-        self.rect = pygame.Rect(32, 32, 16, 16)
-
-        # self.speedx = 0
-        # self.speedy = 0
+        self.rect = pygame.Rect(32, 32, tank_size, tank_size)
 
     def move(self, direction_x, direction_y):
 
@@ -59,10 +48,6 @@ class Player(object):
             self.move_single_axis(direction_x, 0)
         if direction_y != 0:
             self.move_single_axis(0, direction_y)
-
-
-
-
 
     def move_single_axis(self, direction_x, direction_y):
 
@@ -84,47 +69,34 @@ class Player(object):
         global bulets
         bulets.append(Bulet(self.rect.x, self.rect.y, direction_x, direction_y))
 
-    # def shoot(self):
-    #     bullet = Bulet(self.rect.centerx, self.rect.top)
-    #     # all_sprites.add(bullet)
-    #     bulets.add(bullet)
 
 class Bulet():
     def __init__(self, x, y, direction_x, direction_y):
-        # pygame.sprite.Sprite.__init__(self)
-        # self.image = pygame.Surface((10, 20))
-        # self.image.fill(green)
-        # self.rect = self.image.get_rect()
-        # self.rect.bottom = screen_width
-        # self.rect.centerx = 0
-        # self.speed = 10
-
-
         self.bulet = pygame.image.load('bulet')
-        self.rect = pygame.Rect(x, y,8 ,8 )
+        self.rect = pygame.Rect(x, y, 8, 8)
         self.x = x
         self.y = y
         self.direction_x = direction_x
         self.direction_y = direction_y
 
     def update(self):
-        self.rect.x += self.direction_x *5
-        self.rect.y += self.direction_y *5
+        self.rect.x += self.direction_x * 5
+        self.rect.y += self.direction_y * 5
 
 
-# Nice class to hold a wall rect
 class Wall(object):
     def __init__(self, pos):
         walls.append(self)
-        self.rect = pygame.Rect(pos[0], pos[1], 16, 16)
+        self.rect = pygame.Rect(pos[0], pos[1], block_size, block_size)
 
 
 class Background(pygame.sprite.Sprite):
     def __init__(self, image_file, location):
-        pygame.sprite.Sprite.__init__(self)  #call Sprite initializer
+        pygame.sprite.Sprite.__init__(self)  # call Sprite initializer
         self.image = pygame.image.load(image_file)
         self.rect = self.image.get_rect()
         self.rect.left, self.rect.top = location
+
 
 def text_objects(text, color, size):
     if size == "small":
@@ -138,23 +110,23 @@ def text_objects(text, color, size):
 
 def message_to_screen(msg, color, x_displace=0, size="small"):
     text_surface, text_rect = text_objects(msg, color, size)
-    text_rect.center = (screen_height -450) + x_displace, (screen_width -240)
+    text_rect.center = (screen_height - 450) + x_displace, (screen_width - 240)
     screen.blit(text_surface, text_rect)
 
 
-class dummysound:
+class Dummysound:
     def play(self): pass
 
 
 def load_sound(file):
-    if not pygame.mixer: return dummysound()
+    if not pygame.mixer: return Dummysound()
     file = os.path.join(main_dir, 'data', file)
     try:
         sound = pygame.mixer.Sound(file)
         return sound
     except pygame.error:
-        print ('Warning, unable to load, %s' % file)
-    return dummysound()
+        print('Warning, unable to load, %s' % file)
+    return Dummysound()
 
 
 pygame.init()
@@ -176,13 +148,14 @@ screen_width = 800
 screen_height = 600
 screen = pygame.display.set_mode((screen_width, screen_height))
 
-
 clock = pygame.time.Clock()
 walls = []  # List to hold the walls
+block_size = 16  # All blocks size
+tank_size = 26
 
 bulets = []
 player = Player()  # Create the player
-enemy = Enemy(-2,0)
+enemy = Enemy(-2, 0)
 
 bulet_x = 2
 bulet_y = 0
@@ -197,8 +170,6 @@ brick = pygame.image.load('brick.png')
 enem_tank = enem_tank_img
 tank = img_tank
 
-
-
 # all_sprites = pygame.sprite.Group()
 # all_sprites.add(player)
 BackGround = Background('background1.png', [0, 0])
@@ -209,23 +180,19 @@ for row in level:
         if col == "W":
             Wall((x, y))
         if col == "E":
-            end_rect = pygame.Rect(x, y, 16, 16)
-        x += 16
-    y += 16
+            end_rect = pygame.Rect(x, y, block_size, block_size)
+        x += block_size
+    y += block_size
     x = 0
-
-
 
 if pygame.mixer:
     music = os.path.join(main_dir, 'data', 'house_lo.wav')
     pygame.mixer.music.load(music)
     pygame.mixer.music.play(-1)
 
-
 if pygame.mixer and not pygame.mixer.get_init():
     print('Warning, no sound')
     pygame.mixer = None
-
 
 if pygame.mixer:
     music = os.path.join(main_dir, 'data', 'house_lo.wav')
@@ -235,30 +202,11 @@ running = True
 while running:
     clock.tick(32)
 
-
-
-
     for e in pygame.event.get():
         if e.type == pygame.QUIT:
             running = False
         if e.type == pygame.KEYDOWN and e.key == pygame.K_ESCAPE:
             running = False
-
-
-
-    # Movement
-
-    # for event in pygame.event.get():
-    #     if event.type == pygame.KEYDOWN:
-    #         if event.key == pygame.K_LEFT:
-    #             player.move(-2, 0)
-    #         if event.key == pygame.K_RIGHT:
-    #             player.move(2, 0)
-    #         if event.key == pygame.K_UP:
-    #             player.move(0, -2)
-    #         if event.key == pygame.K_DOWN:
-    #             player.move(0, 2)
-
 
     key = pygame.key.get_pressed()
     direction_x = 0
@@ -288,53 +236,59 @@ while running:
     if key[pygame.K_SPACE]:
         player.shoot(bulet_x, bulet_y)
 
-
     # Draw the scene
     screen.fill((0, 0, 0))
     screen.blit(BackGround.image, BackGround.rect)
+    # Draw walls
     for wall in walls:
         screen.blit(brick, wall.rect)
-        #pygame.draw.rect(screen, (255, 255, 255), wall.rect)
-    pygame.draw.rect(screen, black, end_rect)
 
-    #pygame.draw.rect(screen, (255, 200, 0), player.rect)
+    pygame.draw.rect(screen, black, end_rect)
 
     screen.blit(tank, player.rect)
 
-
     if enemy.alive:
         screen.blit(enem_tank, enemy.rect)
-        #pygame.draw.rect(screen, red, enemy.rect)
+        # pygame.draw.rect(screen, red, enemy.rect)
         enemy.move_single_axis()
     else:
         kills += 1
-        dir_x = random.randint(-2,2)
-        dir_y = random.randint(-2,2)
-        enemy = Enemy(dir_x, dir_y)
-        #enem_tank_img
-        if dir_x < 0 :
-            enem_tank = pygame.transform.rotate(enem_tank_img, 180)
-        if dir_x > 0 :
-            enem_tank = enem_tank_img
-        if dir_y < 0 :
-            enem_tank = pygame.transform.rotate(enem_tank_img, 90)
-        if dir_y > 0 :
-            enem_tank = pygame.transform.rotate(enem_tank_img, 270)
+        enemy_move_speed = (-2, -1, 1, 2)
+        dir_x = random.sample(enemy_move_speed, 1)
+        dir_y = random.sample(enemy_move_speed, 1)
 
+        if dir_x != dir_y:
+            dir_y[0] = 0
+        else:
+            dir_x[0] = 0
+
+        dir_x = dir_x[0]
+        dir_y = dir_y[0]
+
+        enemy = Enemy(dir_x, dir_y)
+        # enem_tank_img
+        if dir_x < 0:
+            enem_tank = pygame.transform.rotate(enem_tank_img, 180)
+        if dir_x > 0:
+            enem_tank = enem_tank_img
+        if dir_y < 0:
+            enem_tank = pygame.transform.rotate(enem_tank_img, 90)
+        if dir_y > 0:
+            enem_tank = pygame.transform.rotate(enem_tank_img, 270)
 
     # all_sprites.draw(screen)
     for bulet in bulets:
         screen.blit(bulet_img, bulet.rect)
-        #pygame.draw.rect(screen, green, bulet.rect)
+        # pygame.draw.rect(screen, green, bulet.rect)
         bulet.update()
 
         if bulet.rect.x + 8 >= enemy.rect.x \
-                and bulet.rect.x < enemy.rect.x + 16 \
+                and bulet.rect.x < enemy.rect.x + block_size \
                 and bulet.rect.y + 8 >= enemy.rect.y \
-                and bulet.rect.y < enemy.rect.y + 16:
+                and bulet.rect.y < enemy.rect.y + block_size:
             enemy.distroy()
     message_to_screen("You have kiled: ", yelow)
-    message_to_screen(str(kills), yelow, 70, size="medium")
+    message_to_screen(str(kills), yelow, 100, size="medium")
     pygame.display.flip()
 
 if pygame.mixer:
